be9cfb9a9e6e12597707845641f5494fe5ce6a9d
Chan Kok Wai <chankokwai@Chans-MacBook-Air.local>
2024-05-15 12:18:09 +0800

    changes made

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-## testing
+# Setting up Spark
+Versions are to be checked in the official webpage on `Downloading` section.
+## References
+- [Spark Official Webpage](https://spark.apache.org/docs/latest/quick-start.html)
+
+### REQUIRED Dependencies
+- [Spark Core](https://mvnrepository.com/artifact/org.apache.spark/spark-core)
+- [Spark Project SQL](https://mvnrepository.com/artifact/org.apache.spark/spark-sql)
+- [Apache Hadoop HDFS](https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-hdfs)
+
+### Testing Spark
+1. A list is created with hard-coded data(s) and add some data into the list.
+<br>
+```
+List<Double> myList = new ArrayList<>();
+myList.add(12.134);
+.......
+.......
+N(times)
+```
+
+2. Spark Configuration, making connection to 
+Spark Cluster and close connection.
+```
+SparkConf conf = new SparkConf();
+JavaSparkContext sc = new JavaSparkContext(conf);
+
+sc.close();
+```
+3. Load the collection of data and turning it into a RDD.
+Initialize it to myRDD.
+```
+JavaRDD myRdd = sc.parallelize(myList);
+```
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,42 @@
 package org.example;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
     *
     * @project Spark in Java Practice
     * @author chankokwai
     * @date 15/5/24
     *
-   */public class Main {
+   */
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Double> myList = new ArrayList<>();
+        myList.add(12.231);
+        myList.add(1.54);
+        myList.add(5.321);
+        myList.add(40.56);
+
+        //Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf()
+                .setAppName("starting_spark")
+                .setMaster("local[*]");
+                //.set("spark.driver.bindAddress", "127.0.0.1");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Double> myRDD = sc.parallelize(myList);
+
+        sc.close();
     }
 }
