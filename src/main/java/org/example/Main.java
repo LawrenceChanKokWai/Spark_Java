1273c0571f454d44833cfd9dd693ce7bbbf3aa61
Chan Kok Wai <chankokwai@Chans-MacBook-Air.local>
2024-05-15 10:53:16 +0800

    initial commit to master

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+    *
+    * @project Spark in Java Practice
+    * @author chankokwai
+    * @date 15/5/24
+    *
+   */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
